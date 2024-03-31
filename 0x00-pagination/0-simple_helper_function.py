b30270a226e32d81faa6dd03907f7b2f7963bab2
mcrymbo <croco2023@gmail.com>
2024-03-31 08:35:31 +0300

    index range

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env/ python3
+"""
+returns a tuple containing start and end index
+"""
+from typing import Tuple
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """ function that return start and end index """
+    return ((page - 1) * page_size, page * page_size)
