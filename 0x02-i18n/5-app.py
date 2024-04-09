aa1bd54ece5c767acfb01ac8eacc6b8983e25f47
Mcrymbo <croco2023@gmail.com>
2024-04-09 16:08:17 +0000

    mock login

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python3
-""" Basic flask app """
-from flask import Flask, render_template, request
+""" basic flask app """
+from flask import Flask, render_template, request, g
 from flask_babel import Babel
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
 
 app = Flask(__name__)
 babel = Babel(app)
 
 
 class Config(object):
-    """ Languages and default settings class """
+    """ Available languages class """
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
@@ -19,20 +27,34 @@ app.config.from_object(Config)
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def index() -> str:
-    """ default route
+    """ Default route   
     """
     return render_template('5-index.html')
 
 
 @babel.localeselector
-def get_locale() -> str:
-    """ Determines best languages """
-    if request.args.get('locale'):
-        locale = request.args.get('locale')
-        if locale in app.config['LANGUAGES']:
-            return locale
-    else:
-        return request.accept_languages.best_match(app.config['LANGUAGES'])
+def get_locale():
+    ''' get locale from request '''
+    locale = request.args.get("locale")
+    if locale:
+        return locale
+    return request.accept_languages.best_match(Config.LANGUAGES)
+
+
+def get_user() -> Union[dict, None]:
+    """ Returns user dict if ID can be found """
+    login_as = request.args.get("login_as", False)
+    if login_as:
+        user = users.get(int(login_as), False)
+        if user:
+            return user
+    return None
+
+
+@app.before_request
+def before_request():
+    """ Finds user and sets as global on flask.g.user """
+    g.user = get_user()
 
 
 if __name__ == "__main__":
